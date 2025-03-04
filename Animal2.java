b58518b911ef9ab8fcb0a113a0fb3b17ac355abd
shivam88309 <shivampatelsp8989@gmail.com>
2025-03-04 11:16:36 +0530

    Add files via upload

---
+++ Animal2.java
@@ -0,0 +1,12 @@
+//COLLECTION IN JAVA:LIST
+import java.util.List;
+import java.util.ArrayList;
+public class Animal2 {
+    public static void main(String[] args) {
+        List<String> animal2 = new ArrayList<>();//creating list
+        animal2.add("Ant");
+        animal2.add("Tiger");
+        animal2.add("Lion");
+        System.out.println("My animal2 : "+animal2);
+    }
+}
